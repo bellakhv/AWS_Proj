fa819900561a4b47ff8501aa1faf99f936750b10
bellakhv <61415767+bellakhv@users.noreply.github.com>
2020-03-10 14:12:14 +0200

    Update AWS_task2.py

--- AWS_task2.py
+++ AWS_task2.py
@@ -2,8 +2,8 @@
 I didn't had enough time to complete this task due to env issues.
 
 If i had time I would write another script that uses the function ec2.monitor_instances(InstanceIds=['INSTANCE_ID'])
-to monitor the instance and find the best time for instance stop, and also print the info of the spot price that i have
-found in the previous script and if it pass the trash hold.
+to monitor the instance and find the best time for instance stop on the main process, and also print the info of the spot price that i have
+found in the previous script and if it pass the trash hold using a difrent thread.
 
 the current price is in the SpotPriceHistory in the response and the current price i'm paying is the SpotPrice from the
 same response.
